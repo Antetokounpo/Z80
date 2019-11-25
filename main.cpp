547a74b4440ed73aca5097b0ea8e9b92d51b1181
Antetokounpo <antor.232@outlook.com>
2019-11-25 14:09:20 -0500

    Add build script

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "z80.hpp"
 #include<iostream>
 
-
-int main()
+int main(int argc, char* argv[])
 {
+    if(argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " [filename]\n";
+        return -1;
+    }
+
     Z80::Z80 cpu;
 
-    cpu.load("ti83plus.rom");
+    cpu.load(argv[1]);
     while(true)
     {
         cpu.step();
